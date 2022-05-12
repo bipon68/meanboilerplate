c3e5af994524228d3c1b0cd6a8187e0e32aa17c0
bipon68 <bipon68@users.noreply.github.com>
2022-05-13 02:20:45 +0600

    #1 billing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,17 +8,20 @@ import { LoginComponent } from './login/login.component';
 // import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SettingsComponent } from './settings/settings.component';
+import { BillingComponent } from './billing/billing.component';
 
 @NgModule({
-  declarations: [				
+  declarations: [					
     AppComponent,
       LoginComponent,
       ProfileComponent,
-      SettingsComponent
+      SettingsComponent,
+      BillingComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
