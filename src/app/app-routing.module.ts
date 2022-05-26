27b28e87dc76bb097d12450c4edf0e88576eda94
bipon68 <bipon68@users.noreply.github.com>
2022-05-27 01:08:59 +0600

    billing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { SettingsComponent } from './settings/settings.component';
 import { LoginComponent } from './login/login.component';
+import { BillingComponent } from './billing/billing.component';
 
 const routes: Routes = [
   {path: 'profile', component:ProfileComponent},
   {path: 'settings', component: SettingsComponent},
+  {path: 'billing', component: BillingComponent},
   {
     path: 'home',
     loadChildren: () => import('./_modules/home/home.module').then(m => m.HomeModule)
